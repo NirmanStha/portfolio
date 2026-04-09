10755862fa119d090b887f056fe9ae88b2b464fa
Nirman Shrestha <nirmans39@gmail.com>
2026-04-09 17:37:11 +0000

    seo optimization

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+export const NEXT_PUBLIC_SITE_URL =
+  process.env.NEXT_PUBLIC_SITE_URL || "https://nirman-shrestha.com.np";
+export const NEXT_PUBLIC_GITHUB_URL =
+  process.env.NEXT_PUBLIC_GITHUB_URL || "https://github.com/NirmanStha";
+export const NEXT_PUBLIC_LINKEDIN_URL =
+  process.env.NEXT_PUBLIC_LINKEDIN_URL ||
+  "https://www.linkedin.com/in/nirman-shrestha/";
